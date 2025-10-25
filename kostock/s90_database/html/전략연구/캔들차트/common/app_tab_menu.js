61a344349e3c2ee645fc2642f55e2a262ecd33bb
Jace Kim <jacekim.theal@gmail.com>
2025-10-25 20:33:29 +0900

    문서자료

--- kostock/s90_database/html/전략연구/캔들차트/common/app_tab_menu.js
+++ kostock/s90_database/html/전략연구/캔들차트/common/app_tab_menu.js
@@ -1,3 +1,19 @@
+tab_url_list = [
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/candle_chart.html',
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/candle_pattern1.html',
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/candle_pattern2.html',
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/candle_pattern3.html',
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/candle_pattern4.html',
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/candle_pattern5.html',
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/candle_pattern6.html',
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/candle_pattern7.html',
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/candle_usuage.html'
+];
+
+  function navigateToTab(index) {
+    window.location.href = tab_url_list[index];
+  }
+
   function showChapter(index) {
     const contents = document.querySelectorAll('.tab-content');
     const buttons = document.querySelectorAll('.tab-header button');
@@ -10,3 +26,6 @@
       b.classList.toggle('active', i === index);
     });
   }
+
+  
+  

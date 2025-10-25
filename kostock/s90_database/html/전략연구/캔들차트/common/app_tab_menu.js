a037904ff998754d28431f02f2057a625dbd1e81
Jace Kim <jacekim.theal@gmail.com>
2025-10-26 01:27:51 +0900

    문서자료

--- kostock/s90_database/html/전략연구/캔들차트/common/app_tab_menu.js
+++ kostock/s90_database/html/전략연구/캔들차트/common/app_tab_menu.js
@@ -10,8 +10,21 @@ tab_url_list = [
   'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/candle_usage.html'
 ];
 
+tab_file_list = [
+  './candle_chart.html',
+  './candle_pattern1.html',
+  './candle_pattern2.html',
+  './candle_pattern3.html',
+  './candle_pattern4.html',
+  './candle_pattern5.html',
+  './candle_pattern6.html',
+  './candle_pattern7.html',
+  './candle_usage.html'
+];
+
   function navigateToTab(index) {
     window.location.href = tab_url_list[index];
+    // window.location.href = tab_file_list[index];
   }
 
   function showChapter(index) {

2c50226f2d2136db12817892944afd343da0e653
Jace Kim <jacekim.theal@gmail.com>
2026-07-28 02:10:20 +0900

    고수탐방

--- kostock/s10_experts/LEC_천리안/html/S0/common/app_tab_menu.js
+++ kostock/s10_experts/LEC_천리안/html/S0/common/app_tab_menu.js
@@ -1,25 +1,25 @@
 tab_url_list = [
-  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/candle_chart.html',
-  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/candle_pattern1.html',
-  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/candle_pattern2.html',
-  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/candle_pattern3.html',
-  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/candle_pattern4.html',
-  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/candle_pattern5.html',
-  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/candle_pattern6.html',
-  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/candle_pattern7.html',
-  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/candle_usage.html'
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s10_experts/LEC_천리안/html/S0/천리안주식특강.html',
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s10_experts/LEC_천리안/html/S0/N04_낙폭과대매매.html',
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s10_experts/LEC_천리안/html/S0/N05_추세추종눌림.html',
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s10_experts/LEC_천리안/html/S0/N06_추세추종단스.html',
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s10_experts/LEC_천리안/html/S0/N07_리뉴얼매매법.html',
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s10_experts/LEC_천리안/html/S0/N01_시장먼저읽기.html',
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s10_experts/LEC_천리안/html/S0/N02_시소타기장세.html',
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s10_experts/LEC_천리안/html/S0/N03_종목선정실전.html',
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/biz_finance/master/kostock/s90_database/html/전략연구/캔들차트/마지막노트.html'
 ];
 
 tab_file_list = [
-  './candle_chart.html',
-  './candle_pattern1.html',
-  './candle_pattern2.html',
-  './candle_pattern3.html',
-  './candle_pattern4.html',
-  './candle_pattern5.html',
-  './candle_pattern6.html',
-  './candle_pattern7.html',
-  './candle_usage.html'
+  './천리안주식특강.html',
+  './N04_낙폭과대매매.html',
+  './N05_추세추종눌림.html',
+  './N06_추세추종단스.html',
+  './N07_리뉴얼매매법.html',
+  './N01_시장먼저읽기.html',
+  './N02_시소타기장세.html',
+  './N03_종목선정실전.html',
+  './마지막노트.html'
 ];
 
   function navigateToTab(index) {
